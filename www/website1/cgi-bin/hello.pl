1f52e30631129f1dbec7a23dd18c8a1495dba7d8
NomanMunir <mrnomanmunir@gmail.com>
2024-09-20 00:12:34 +0400

    - Added some static game and website in www folder.

--- www/website1/cgi-bin/hello.pl
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/perl
-print "<html><body><h1>Hello, World from Perl!</h1></body></html>";
--- www/website1/cgi-bin/infinite.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while (true); do
-    echo "This is an infinite loop"
-    sleep 1  # Pause for 1 second to avoid high CPU usage
-done
